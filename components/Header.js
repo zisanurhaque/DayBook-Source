99fa5075eef0c4125a814d53ca900265c2144fb0
Zisanur Haque <52825814+zisanurhaque@users.noreply.github.com>
2021-12-24 15:32:37 +0600

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <>
+                <div className="header">
+                    <div className="brand">
+                        <h4>Jabeda</h4>
+                    </div>
+                    <p>Powered By <a href="https://zisanurhaque.com">Zisanur Haque</a></p>
+                </div>
+        </>
+    )
+}
+
+export default Header;
